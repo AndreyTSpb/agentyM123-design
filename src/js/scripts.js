6ac8d5a792793de52654d268cdba071a1c1bcf7a
Andrey <tynyanyi@mail.ru>
2020-06-16 19:58:53 +0300

    slick slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -6,8 +6,30 @@ $(document).ready(function(){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        infinite: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left-arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>',
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
     });
     //Слайдер с субновостями 
     $('.news-subnews').slick({
