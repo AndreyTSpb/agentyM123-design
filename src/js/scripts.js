7995b1d23aa1e60e6a3cbc11eacbef9c728ab59f
Andrey <tynyanyi@mail.ru>
2020-06-16 22:06:12 +0300

    стилизация для телефона

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,32 +4,37 @@ $(document).ready(function(){
     $('.magazines-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
+        initialSlide: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        infinite: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left-arrow.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>',
         responsive: [
             {
-              breakpoint: 1025,
-              settings: {
-                slidesToShow: 3
-              }
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                }
             },
             {
-              breakpoint: 768,
-              settings: {
-                slidesToShow: 2
-              }
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                }
             },
             {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    slidesToScroll: 2,
+                }
             }
-          ]
+        ]
     });
     //Слайдер с субновостями 
     $('.news-subnews').slick({
@@ -38,6 +43,32 @@ $(document).ready(function(){
         autoplay: true,
         autoplaySpeed: 2000,
         prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/left-arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/right-arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    initialSlide: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    initialSlide: 1,
+                }
+            },
+            {
+                breakpoint: 360,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    slidesToScroll: 2,
+                }
+            }
+        ]
     });
   });
