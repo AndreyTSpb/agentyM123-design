b53a9a16e9751dfd8f0e76a54348909474689c48
Andrey <tynyanyi@mail.ru>
2020-07-18 15:57:55 +0300

    about the club

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -186,6 +186,40 @@ $(document).ready(function(){
             }
         ]
     });
+
+
+    $('.raund-gallery').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/grey-left-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/icons/grey-right-arrow.png"></button>',
+        
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+        
+      });
+
     //инициализация подсказок средствами bootstrap
     $('[data-toggle="tooltip"]').tooltip();
 
